5c5429a2e90965c704cacd0fc9d7eaec945d0f80
Koki Kato <koki.kato1994@gmail.com>
2019-10-14 14:46:29 +0900

    Fix bugs of RegularSampler

--- src/suffix_array.rs
+++ src/suffix_array.rs
@@ -21,6 +21,7 @@ pub struct RegularSampledArray {
 
 impl PartialArray for RegularSampledArray {
     fn get(&self, i: u64) -> Option<u64> {
+        debug_assert!(i < self.len as u64);
         if i & ((1 << self.level) - 1) == 0 {
             Some(self.sa.get_word(i as usize >> self.level, self.word_size))
         } else {
@@ -78,13 +79,14 @@ impl ArraySampler<RegularSampledArray> for RegularSampler {
     fn sample(&self, sa: Vec<u64>) -> RegularSampledArray {
         let n = sa.len();
         let word_size = (util::log2(n as u64) + 1) as usize;
+        debug_assert!(n > 0);
         debug_assert!(
             n > (1 << self.level),
             "sampling level L must satisfy 2^L < text_len (L = {}, text_len = {})",
             self.level,
             n,
         );
-        let sa_samples_len = n >> self.level;
+        let sa_samples_len = ((n - 1) >> self.level) + 1;
         let mut sa_samples = fid::BitArray::with_word_size(word_size, sa_samples_len);
         for i in 0..sa_samples_len {
             sa_samples.set_word(i, word_size, sa[i << self.level] as u64);
@@ -94,6 +96,37 @@ impl ArraySampler<RegularSampledArray> for RegularSampler {
             word_size,
             sa: sa_samples,
             len: sa.len(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_regular() {
+        let cases = [
+            (1, 10),
+            (1, 25),
+            (2, 8),
+            (2, 9),
+            (2, 10),
+            (2, 25),
+            (3, 24),
+            (3, 25),
+        ];
+        for &(level, n) in cases.iter() {
+            let sa = (0..n).collect::<Vec<u64>>();
+            let ssa = RegularSampler::new().level(level).sample(sa);
+            for i in 0..n {
+                let v = ssa.get(i);
+                if i & ((1 << level) - 1) == 0 {
+                    assert_eq!(v, Some(i), "ssa[{}] should be Some({})", i, i);
+                } else {
+                    assert_eq!(v, None, "ssa[{}] should be None", i);
+                }
+            }
         }
     }
 }
