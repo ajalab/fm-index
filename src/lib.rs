5285945d2434723dce74ae0ae7456068f7c88870
Koki Kato <koki.kato1994@gmail.com>
2019-10-17 23:57:04 +0900

    Write doc

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@
 //! - `count`: Given a pattern string, counts the number of its occurrences.
 //! - `locate`: Given a pattern string, lists the all position of its occurrences.
 //! - `extract`: Given an integer, gets the character of the text at that position.
+//!
+//! `fm-index` crate does not support the third query (extracting a character from arbitrary position) now.
+//! Instead, it provides an extraction query from a search result.
 
 #![allow(clippy::len_without_is_empty)]
 
