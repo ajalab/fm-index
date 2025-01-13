d0106a255b8dd44165d07fb09ba31b348d197055
Martijn Faassen <faassen@startifact.com>
2025-01-13 15:34:34 +0100

    Unused import.

--- src/sais.rs
+++ src/sais.rs
@@ -1,9 +1,6 @@
 //! SA-IS implementation based on
 //!    Ge Nong, Sen Zhang, & Wai Hong Chan. (2010). Two Efficient Algorithms for Linear Time Suffix Array Construction.
 //!    IEEE Transactions on Computers, 60(10), 1471–1484. <https://doi.org/10.1109/tc.2010.188>
-
-use std::fmt::Debug;
-
 use vers_vecs::BitVec;
 
 use crate::{
