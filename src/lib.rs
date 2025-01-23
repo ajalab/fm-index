d54da80a99276926819e066b573054c41d850b60
Koki Kato <koki.kato1994@gmail.com>
2025-01-23 09:29:39 +0900

    Drop `SearchIndexBuilder`

--- src/lib.rs
+++ src/lib.rs
@@ -152,4 +152,4 @@ pub use crate::rlfmi::RLFMIndex;
 
 pub use character::Character;
 pub use iter::{BackwardIterator, FMIndexBackend, ForwardIterator};
-pub use search::{Search, SearchIndex, SearchIndexBuilder};
+pub use search::{Search, SearchIndex};
