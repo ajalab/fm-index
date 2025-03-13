2442ef3bc58aa216c170b1b7a733feddddf3f9c8
Koki Kato <koki.kato1994@gmail.com>
2025-03-13 09:57:56 +0900

    Fix tests for multi-text

--- tests/testutil/mod.rs
+++ tests/testutil/mod.rs
@@ -1,25 +1,24 @@
 use num_traits::Zero;
 
 /// Build a text for tests using a generator function `gen`.
-pub(crate) fn build_text<T: Zero, F: FnMut() -> T>(mut gen: F, len: usize) -> Vec<T> {
-    let mut text = (0..(len - 1)).map(|_| gen()).collect::<Vec<_>>();
+pub fn build_text<T: Zero + Clone, F: FnMut() -> T>(mut gen: F, len: usize) -> Vec<T> {
+    let mut text = vec![T::zero(); len];
 
-    // Truncate the trailing zeros, since SA-IS does not support trailing zero suffix longer than 1.
-    if let Some(pos) = text.iter().rposition(|x| !x.is_zero()) {
-        text.truncate(pos + 1);
-    } else {
-        text.clear();
-    }
-
-    // Add non-zero elements until the text length reaches len - 1.
-    while text.len() < len - 1 {
-        let c = gen();
-        if !c.is_zero() {
-            text.push(c);
+    let mut prev_zero = true;
+    for t in text.iter_mut().take(len - 1) {
+        let mut c = gen();
+        if prev_zero {
+            while c.is_zero() {
+                c = gen();
+            }
         }
+        prev_zero = c.is_zero();
+        *t = c;
     }
 
-    // Add the last zero as a sentinel for SA-IS.
-    text.push(T::zero());
+    while text[len - 2].is_zero() {
+        text[len - 2] = gen();
+    }
+
     text
 }
