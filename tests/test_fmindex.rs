3b8bb8b7146a9bce943421e8c0dc019a54286001
Koki Kato <koki.kato1994@gmail.com>
2025-04-01 20:53:47 +0900

    Unify e2e test strategy

--- tests/test_fmindex.rs
+++ tests/test_fmindex.rs
@@ -1,1 +1,71 @@
+mod testutil;
+use fm_index::converter::IdConverter;
+use fm_index::{FMIndexWithLocate, MatchWithLocate, Search};
+use testutil::TestRunner;
 
+#[test]
+fn test_search_count() {
+    let text_size = 1024;
+
+    TestRunner {
+        texts: 100,
+        patterns: 100,
+        text_size,
+        alphabet_size: 8,
+        level_max: 3,
+        pattern_size_max: 10,
+        multi_text: false,
+    }
+    .run(
+        |text, level| FMIndexWithLocate::new(text, IdConverter::new::<u8>(), level),
+        |fm_index, text, pattern| {
+            let naive_index = testutil::NaiveSearchIndex::new(text);
+            let matches_expected = naive_index.search(pattern);
+
+            let count_expected = matches_expected.len() as u64;
+            let count_actual = fm_index.search(pattern).count();
+            assert_eq!(
+                count_expected, count_actual,
+                "text = {:?}, pattern = {:?}",
+                text, pattern
+            );
+        },
+    );
+}
+#[test]
+fn test_search_locate() {
+    let text_size = 100;
+
+    TestRunner {
+        texts: 100,
+        patterns: 100,
+        text_size,
+        alphabet_size: 8,
+        level_max: 3,
+        pattern_size_max: 10,
+        multi_text: false,
+    }
+    .run(
+        |text, level| FMIndexWithLocate::new(text, IdConverter::new::<u8>(), level),
+        |fm_index, text, pattern| {
+            let naive_index = testutil::NaiveSearchIndex::new(text);
+            let matches_expected = naive_index.search(pattern);
+
+            let positions_expected = matches_expected
+                .iter()
+                .map(|m| m.position)
+                .collect::<Vec<_>>();
+            let mut positions_actual = fm_index
+                .search(pattern)
+                .iter_matches()
+                .map(|m| m.locate())
+                .collect::<Vec<_>>();
+            positions_actual.sort();
+            assert_eq!(
+                positions_expected, positions_actual,
+                "text = {:?}, pattern = {:?}",
+                text, pattern
+            );
+        },
+    );
+}
--- tests/testutil/mod.rs
+++ tests/testutil/mod.rs
@@ -1,3 +1,4 @@
+use fm_index::TextId;
 use num_traits::Zero;
 use rand::rngs::StdRng;
 use rand::{Rng, SeedableRng};
@@ -25,6 +26,63 @@ pub fn build_text<T: Zero + Clone, F: FnMut() -> T>(mut gen: F, len: usize) -> V
     text
 }
 
+pub struct NaiveSearchIndex<'a> {
+    text: &'a [u8],
+}
+
+impl<'a> NaiveSearchIndex<'a> {
+    pub fn new(text: &'a [u8]) -> Self {
+        NaiveSearchIndex { text }
+    }
+
+    pub fn search(&self, pattern: &[u8]) -> Vec<NaiveSearchIndexMatch> {
+        self.do_search(pattern, false, false)
+    }
+
+    pub fn search_prefix(&self, pattern: &[u8]) -> Vec<NaiveSearchIndexMatch> {
+        self.do_search(pattern, true, false)
+    }
+
+    pub fn search_suffix(&self, pattern: &[u8]) -> Vec<NaiveSearchIndexMatch> {
+        self.do_search(pattern, false, true)
+    }
+
+    pub fn search_exact(&self, pattern: &[u8]) -> Vec<NaiveSearchIndexMatch> {
+        self.do_search(pattern, true, true)
+    }
+
+    fn do_search(
+        &self,
+        pattern: &[u8],
+        match_prefix_only: bool,
+        match_suffix_only: bool,
+    ) -> Vec<NaiveSearchIndexMatch> {
+        let mut result = Vec::new();
+        let mut text_id = 0;
+        for i in 0..self.text.len() - pattern.len() + 1 {
+            if self.text[i] == 0 {
+                text_id += 1;
+            }
+            if (!match_prefix_only || (i == 0 || self.text[i - 1] == 0))
+                && (!match_suffix_only
+                    || (i + pattern.len() == self.text.len() || self.text[i + pattern.len()] == 0))
+                && &self.text[i..i + pattern.len()] == pattern
+            {
+                result.push(NaiveSearchIndexMatch {
+                    position: i as u64,
+                    text_id: TextId::from(text_id),
+                });
+            }
+        }
+        result
+    }
+}
+
+pub struct NaiveSearchIndexMatch {
+    pub position: u64,
+    pub text_id: TextId,
+}
+
 pub struct TestRunner {
     pub texts: usize,
     pub patterns: usize,
@@ -32,6 +90,7 @@ pub struct TestRunner {
     pub alphabet_size: u8,
     pub level_max: usize,
     pub pattern_size_max: usize,
+    pub multi_text: bool,
 }
 
 impl TestRunner {
@@ -43,7 +102,11 @@ impl TestRunner {
         let mut rng = StdRng::seed_from_u64(0);
 
         for _ in 0..self.texts {
-            let text = build_text(|| rng.gen::<u8>() % self.alphabet_size, self.text_size);
+            let text = if self.multi_text {
+                build_text(|| rng.gen::<u8>() % self.alphabet_size, self.text_size)
+            } else {
+                build_text(|| rng.gen::<u8>() % self.alphabet_size + 1, self.text_size)
+            };
             let level = rng.gen::<usize>() % (self.level_max + 1);
             let fm_index = build_index(text.clone(), level);
 
