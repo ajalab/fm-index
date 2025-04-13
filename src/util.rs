1bd3d45fffe252c2fc1608fa6d1908dd9eaaa337
Koki Kato <koki.kato1994@gmail.com>
2025-04-13 15:01:23 +0900

    Remove util::log2

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,3 @@
-pub fn log2(x: u64) -> u64 {
-    ((std::mem::size_of::<u64>() * 8) as u64) - u64::from(x.leading_zeros()) - 1
-}
-
 pub fn log2_usize(x: usize) -> usize {
     (std::mem::size_of::<usize>() * 8) - (x.leading_zeros() as usize) - 1
 }
@@ -9,16 +5,6 @@ pub fn log2_usize(x: usize) -> usize {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn test_log2() {
-        assert_eq!(log2(2u64), 1);
-        assert_eq!(log2(3u64), 1);
-        assert_eq!(log2(4u64), 2);
-        assert_eq!(log2(5u64), 2);
-        assert_eq!(log2(6u64), 2);
-        assert_eq!(log2(7u64), 2);
-        assert_eq!(log2(8u64), 3);
-    }
 
     #[test]
     fn test_log2_usize() {
