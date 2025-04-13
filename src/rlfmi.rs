3cd688a47cd3fd9e327340b6d66eccf0929b3b1e
Koki Kato <koki.kato1994@gmail.com>
2025-04-13 22:43:31 +0900

    Use len() instead of capacity() in heap_size()

--- src/rlfmi.rs
+++ src/rlfmi.rs
@@ -100,7 +100,7 @@ where
         self.s.heap_size()
             + self.b.heap_size()
             + self.bp.heap_size()
-            + self.cs.capacity() * std::mem::size_of::<u64>()
+            + self.cs.len() * std::mem::size_of::<u64>()
             + self.suffix_array.heap_size()
     }
 }
