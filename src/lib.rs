b711607c544d7f4fdbf2b6bd63651a670f75be92
Koki Kato <koki.kato1994@gmail.com>
2025-04-26 20:43:02 +0900

    Remove duplicated contents from documents

--- src/lib.rs
+++ src/lib.rs
@@ -12,78 +12,9 @@
 //! from arbitrary position). Instead, it provides backward/forward iterators
 //! that return the text characters starting from a search result.
 //!
-//! # Usage
-//!
-//! Add this to your `Cargo.toml`.
-//!
-//! ```toml
-//! [dependencies]
-//! fm-index = "0.2.0"
-//! ```
-//!
-//! # Example
-//! ```
-//! use fm_index::{Search, FMIndexWithLocate, MatchWithLocate, Match, Text};
-//!
-//! // Prepare a text string to search for patterns.
-//! let text = concat!(
-//!     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-//!     "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-//!     "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-//!     "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-//!     "\0",
-//! ).as_bytes();
-//! let text = Text::new(text);
-//!
-//! // To perform locate queries, we need to use some storage.
-//! // the sampling level determines how much is retained in order to support `locate`
-//! // queries. `0` retains the full information, but we don't need the whole array
-//! // since we can interpolate missing elements in a suffix array from others. A sampler
-//! // will _sieve_ a suffix array for this purpose. If you don't need `locate` queries
-//! // you can save the memory by not setting a sampling level.
-//! let index = FMIndexWithLocate::new(&text, 2).unwrap();
-//!
-//! // Search for a pattern string.
-//! let pattern = "dolor";
-//! let search = index.search(pattern);
-//!
-//! // Count the number of occurrences.
-//! let n = search.count();
-//! assert_eq!(n, 4);
-//!
-//! // List the position of all occurrences.
-//! let positions = search.iter_matches().map(|m| m.locate()).collect::<Vec<usize>>();
-//! assert_eq!(positions, vec![246, 12, 300, 103]);
-//!
-//! // Extract preceding characters from a search position.
-//! let mut prefix = search
-//!     .iter_matches()
-//!     .nth(0)
-//!     .unwrap()
-//!     .iter_chars_backward()
-//!     .take(16)
-//!     .collect::<Vec<u8>>();
-//! prefix.reverse();
-//! assert_eq!(prefix, b"Duis aute irure ".to_owned());
-//!
-//! // Extract succeeding characters from a search position.
-//! let postfix = search
-//!     .iter_matches()
-//!     .nth(3)
-//!     .unwrap()
-//!     .iter_chars_forward()
-//!     .take(20)
-//!     .collect::<Vec<u8>>();
-//! assert_eq!(postfix, b"dolore magna aliqua.".to_owned());
-//!
-//! // Search can be chained backward.
-//! let search_chained = search.search("et ");
-//! assert_eq!(search_chained.count(), 1);
-//! ```
-//!
 //! # Implementations
 //!
-//! ## FM-Index
+//! ## FM-Index ([`FMIndex`], [`FMIndexWithLocate`])
 //!
 //! This is a fast implementation of a succinct full-text index. For most use
 //! cases this is the best choice.
@@ -100,7 +31,7 @@
 //!   number of characters smaller than a given character
 //! - a (sampled) suffix array
 //!
-//! ## Run-Length FM-Index
+//! ## Run-Length FM-Index ([`RLFMIndex`], [`RLFMIndexWithLocate`])
 //!
 //! This index is more compact than FM-Index if there are a lot of repeated
 //! patterns in the text. It is slower than FM-Index in general.
@@ -168,3 +99,7 @@ pub use frontend::{
 };
 pub use piece::PieceId;
 pub use text::Text;
+
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
